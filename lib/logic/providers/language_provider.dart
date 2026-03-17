9ee5e68b2956ea2c65377be47e8ae9ea6da0a5d1
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-03-17 13:26:56 +0600

    feat: add language provider

--- lib/logic/providers/language_provider.dart
+++ lib/logic/providers/language_provider.dart
@@ -5,21 +5,8 @@ class LanguageProvider extends ChangeNotifier {
 
   Locale get appLocale => _appLocale;
 
-  /// Changes the application language based on the provided language code.
-  ///
-  /// Supported language codes:
-  /// - 'ru' → Russian
-  /// - 'en' → English
-  /// - any other value → Kyrgyz (default)
-
-  void changeLanguage() {
-    if (_appLocale.languageCode == 'en') {
-      _appLocale = const Locale('ru');
-    } else if (_appLocale.languageCode == 'ru') {
-      _appLocale = const Locale('ky');
-    } else {
-      _appLocale = const Locale('en');
-    }
+  void setLanguage(Locale locale) {
+    _appLocale = locale;
     notifyListeners();
   }
 }
