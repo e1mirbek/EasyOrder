a30659ba46452381b0037abf3566951dc6e994fe
e1mirbek <elmirbekabdymanapov121@gmail.com>
2026-04-13 19:47:31 +0600

    feat: add banner repository for collection get banners

---
+++ lib/features/home/data/repo/banner_repository.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:easy_order/core/errors/failure.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final bannerRepositoryProvider = Provider(
+  (ref) => BannerRepository(firestore: FirebaseFirestore.instance),
+);
+
+class BannerRepository {
+  final FirebaseFirestore _firestore;
+
+  BannerRepository({required FirebaseFirestore firestore})
+    : _firestore = firestore;
+
+  Future<List<String>> fetchBanners() async {
+    try {
+      final snapshot = await _firestore.collection('banners').get();
+
+      // Превращаем документы в список строк (URL картинок)
+      return snapshot.docs.map((doc) => doc['image'] as String).toList();
+    } catch (e) {
+      throw Failure.fromException(e);
+    }
+  }
+}
